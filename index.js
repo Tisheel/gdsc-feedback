ed53f8d1badedc367bcf666a2fe37960e50aa315
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-01-14 12:20:17 +0530

    done

--- index.js
+++ index.js
@@ -14,15 +14,30 @@ const MONGO_URL = process.env.MONGO_URL;
     app.use(express.static(path.join(__dirname, 'public')))
 
     //Connect To Mongo
-    await connectToMongoDB(MONGO_URL, 'Gdsc_feedback')
+    await connectToMongoDB(MONGO_URL, 'Gdsc')
+
+    //Empty Schema
+    const feedbackSchema = new mongoose.Schema({}, { strict: false, timestamps: true })
+    const Feedback = mongoose.model('Feedback', feedbackSchema)
 
     //APIs
-    app.post('/api', (req, res) => {
+    app.post('/api', async (req, res) => {
         try {
-
+            const feedback = await Feedback(req.body)
+            feedback.save()
+            res.redirect('/thankyou')
         } catch (err) {
+            res.send('Somthing went wrong.')
             console.log(err)
         }
+    })
+
+    app.get('/thankyou', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'thankyou.html'))
+    })
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'public', '404.html'))
     })
 
     app.use('/', (req, res, next) => {
